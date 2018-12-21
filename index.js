c6841fd78f027dbc48e2b79b0b38667224099682
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-21 09:41:19 -0500

    client & server synced

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ app.use(
   })
 );
 
+const cheeses = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
+
+app.get( '/api/cheeses', (req, res, next) => {
+  res.status(200).json(cheeses);
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
